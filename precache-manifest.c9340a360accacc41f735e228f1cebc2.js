e2c818838655e1b29ae1284c1a6572d2b04f0df8
Jacob Sommer <jacob.sommer@comcast.net>
2018-12-31 13:07:20 -0800

    Updates

--- precache-manifest.c9340a360accacc41f735e228f1cebc2.js
+++ precache-manifest.c9340a360accacc41f735e228f1cebc2.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/dhsbells/static/js/runtime~main.a33aba85.js"
   },
   {
-    "revision": "4739919c545f64897b60",
-    "url": "/dhsbells/static/js/main.4739919c.chunk.js"
+    "revision": "be2b82d5fd6a0b5494e9",
+    "url": "/dhsbells/static/js/main.be2b82d5.chunk.js"
   },
   {
     "revision": "a228e91d5e5c3998e21e",
     "url": "/dhsbells/static/js/1.a228e91d.chunk.js"
   },
   {
-    "revision": "4739919c545f64897b60",
-    "url": "/dhsbells/static/css/main.8657a61a.chunk.css"
+    "revision": "be2b82d5fd6a0b5494e9",
+    "url": "/dhsbells/static/css/main.4ed8e82c.chunk.css"
   },
   {
-    "revision": "f2f7786f6af51891576b01311dd246fe",
+    "revision": "69b9f18ad5e7d1a5e9ba74b5b11d2bef",
     "url": "/dhsbells/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dhsbells/precache-manifest.811ac8841c519abc525b3640b92a547a.js"
+  "/dhsbells/precache-manifest.c9340a360accacc41f735e228f1cebc2.js"
 );
 
 workbox.clientsClaim();
