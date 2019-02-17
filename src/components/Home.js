74cad2de10aa75c1daaa9ba60956fbb4251eb34b
Jacob Sommer <jacob.sommer@comcast.net>
2019-02-16 20:48:49 -0800

    Styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,6 +11,10 @@ const beforeSchool = (periods, time) => time < parseRawTime(periods[0].start);
 
 const parseRawTime = (t) => parseInt(t.replace(":", ""));
 
+const bottomMargin = {
+  marginBottom: '40px'
+}
+
 function showPeriod() {
   var date = new Date();
   var time = rawTime(date);
@@ -34,14 +38,14 @@ function showPeriod() {
           <div>
             <h3>{s.periods[i].name}</h3>
             <h1>Ends at {convertTo12Hour(s.periods[i].end)}</h1>
-            <h3 id="next-period">Next: {s.periods[i + 1].name} {convertTo12Hour(s.periods[i + 1].start)} - {convertTo12Hour(s.periods[i + 1].end)}</h3>
+            <h3 style={bottomMargin}>Next: {s.periods[i + 1].name} {convertTo12Hour(s.periods[i + 1].start)} - {convertTo12Hour(s.periods[i + 1].end)}</h3>
           </div>
         );
       } else {
         return (
           <div>
             <h3>{s.periods[i].name}</h3>
-            <h1>Ends at {convertTo12Hour(s.periods[i].end)}</h1>
+            <h1 style={bottomMargin}>Ends at {convertTo12Hour(s.periods[i].end)}</h1>
           </div>
         );
       }
@@ -50,7 +54,7 @@ function showPeriod() {
         <div>
           <h3>Passing Period</h3>
           <h1>Ends at {convertTo12Hour(s.periods[i + 1].start)}</h1>
-          <h3 id="next-period">Next: {s.periods[i + 1].name} {convertTo12Hour(s.periods[i + 1].start)} - {convertTo12Hour(s.periods[i + 1].end)}</h3>
+          <h3 style={bottomMargin}>Next: {s.periods[i + 1].name} {convertTo12Hour(s.periods[i + 1].start)} - {convertTo12Hour(s.periods[i + 1].end)}</h3>
         </div>
       );
     }
